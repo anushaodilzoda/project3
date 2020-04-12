dc3aeea79115df73ae32378a9bc6f2ea1e044575
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2020-04-12 03:09:09 -0400

    added template for Q&A

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,18 @@
 import React, { Component } from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   state = {};
   render() {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <Link class="navbar-brand" to="/">
+          Home
+        </Link>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
             <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
@@ -57,16 +37,6 @@ class Navbar extends Component {
                   Something else here
                 </a>
               </div>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -4,7 +4,126 @@ import "../App.css";
 class Posts extends Component {
   state = {};
   render() {
-    return <div>POSTS</div>;
+    return (
+      <div className="content">
+        <div className="row table-header">
+          <div className="col-md-1">#</div>
+          <div className="col-md-9">Questions</div>
+          <div className="col-md-1">User</div>
+          <div className="col-md-1">Date</div>
+        </div>
+
+        <div className="row question-section">
+          <div className="col-md-1">1</div>
+          <div className="col-md-9">
+            What are JavaScript Data Types?
+            <button
+              class="btn btn-outline-primary btn-sm"
+              data-toggle="collapse"
+              data-target="#entry1"
+            >
+              +
+            </button>
+            <div className="answer_section">
+              <div id="entry1" class="collapse mt-2">
+                <div className="entry">
+                  Number, String, Boolean, Object, Undefined
+                </div>
+                <form>
+                  <div class="form-group">
+                    <textarea
+                      class="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                    ></textarea>
+                  </div>
+                  <button type="button" class="btn btn-outline-primary btn-sm">
+                    Enter Solution
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-1">Khaliunaa</div>
+          <div className="col-md-1">04/12/2020</div>
+        </div>
+
+        <div className="row question-section">
+          <div className="col-md-1">2</div>
+          <div className="col-md-9">
+            What is the use of isNaN function?
+            <button
+              class="btn btn-outline-primary btn-sm"
+              data-toggle="collapse"
+              data-target="#entry2"
+            >
+              +
+            </button>
+            <div className="answer_section">
+              <div id="entry2" class="collapse mt-2">
+                <div className="entry">
+                  isNan function returns true if the argument is not a number
+                  otherwise it is false.
+                </div>
+                <form>
+                  <div class="form-group">
+                    <textarea
+                      class="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                    ></textarea>
+                  </div>
+                  <button type="button" class="btn btn-outline-primary btn-sm">
+                    Enter Solution
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-1">Khaliunaa</div>
+          <div className="col-md-1">04/12/2020</div>
+        </div>
+
+        <div className="row question-section">
+          <div className="col-md-1">3</div>
+          <div className="col-md-9">
+            Between JavaScript and an ASP script, which is faster?
+            <button
+              class="btn btn-outline-primary btn-sm"
+              data-toggle="collapse"
+              data-target="#entry3"
+            >
+              +
+            </button>
+            <div className="answer_section">
+              <div id="entry3" class="collapse mt-2">
+                <div className="entry">
+                  JavaScript is faster. JavaScript is a client-side language and
+                  thus it does not need the assistance of the web server to
+                  execute. On the other hand, ASP is a server-side language and
+                  hence is always slower than JavaScript. Javascript now is also
+                  a server side language (nodejs).
+                </div>
+                <form>
+                  <div class="form-group">
+                    <textarea
+                      class="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                    ></textarea>
+                  </div>
+                  <button type="button" class="btn btn-outline-primary btn-sm">
+                    Enter Solution
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-1">Khaliunaa</div>
+          <div className="col-md-1">04/12/2020</div>
+        </div>
+      </div>
+    );
   }
 }
 
