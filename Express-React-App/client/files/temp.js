dc7bcf181f6230bd306fc0fa3963b891f7110c85
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2020-04-13 20:28:07 -0400

    added file

---
+++ Express-React-App/client/files/temp.js
@@ -0,0 +1,14 @@
+export function getAllData() {
+  return datas;
+}
+
+const datas = [
+  {
+    _id: "5b21ca3eeb7f6fbccd471815",
+    text: "#"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471816",
+    text: "#"
+  }
+];
