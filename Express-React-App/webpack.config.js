25bb03122c66a58fa21de5baf9eb49caedce3b71
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2020-05-01 12:59:31 -0400

    added import moment

--- Express-React-App/webpack.config.js
+++
@@ -1,6 +0,0 @@
-plugins: [
-        
-    // Fixes warning in moment-with-locales.min.js 
-    //   Module not found: Error: Can't resolve './locale' in ...
-    new webpack.IgnorePlugin(/\.\/locale$/)
-]
